420d237a6f326be3def4a9b2312e00e804e5f2c5
dev1 <dima.vbg7@gmail.com>
2017-02-24 23:00:31 +0300

    Приветствие слато больше

--- Hello.py
+++ Hello.py
@@ -1,2 +1,12 @@
-print('Hello, world') #Выводим приветствие
+print('    /  \\    ')
+print('   /    \\   ')
+print('  /      \\  ')
+print(' /        \\ ')
+print('/          \\')
+print('|Hello, world|') #Выводим приветствие
+print('\           /')
+print(' \         / ')
+print('  \       /  ')
+print('   \     /   ')
+print('    \___/    ')
 print(123)
